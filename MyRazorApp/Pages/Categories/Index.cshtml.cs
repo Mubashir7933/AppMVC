8eb7d78a02d0b556d70e0377360c44c58e85118d
Mubashir~ <21051350129@ogr.halic.edu.tr>
2025-03-17 22:37:19 +0300

    Displaying Categories using RazorPages

--- MyRazorApp/Pages/Categories/Index.cshtml.cs
+++ MyRazorApp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyRazorApp.Data;
+using MyRazorApp.Models;
 
 namespace MyRazorApp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List <Category> CategoryList {get; set;}
+         public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
--- MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
+++ MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc814cf80c0a235354dec6a219ef22a95e7ab84b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414dcf3323db54390a26047ff5843f23a9a8fcc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
